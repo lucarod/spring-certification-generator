db685625920e3d64c42f3ec26fc3550921badeb5
Luca Rodrigues <luca12rodrigues@gmail.com>
2024-02-10 14:00:06 -0300

    start project folder structure

---
+++ src/main/java/com/lucarod/spring_certification/SpringCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.lucarod.spring_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucarod/spring_certification/SpringCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucarod.spring_certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
